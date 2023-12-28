24b077902fc8eb52110e8d30edf9dd4b1a6d5987
beng2004 <46874844+beng2004@users.noreply.github.com>
2023-12-28 12:14:39 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,89 +3,45 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 import numpy as np
 
-class ImageCropperApp:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Image Cropper App")
+class ImageViewerApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Viewer")
+        self.root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))  # Fullscreen
+
+        self.image_label = tk.Label(self.root)
+        self.image_label.pack(expand="yes", fill="both")
+
+        self.load_button = tk.Button(self.root, text="Load Image", command=self.load_image)
+        self.load_button.pack(side=tk.BOTTOM)
+
+        self.convert_button = tk.Button(self.root, text="Convert and Save", command=self.convert_and_save)
+        self.convert_button.pack(side=tk.BOTTOM)
 
         self.image_path = None
-        self.original_image = None
-        self.cropped_image = None
+        self.current_image = None
 
-        self.canvas = tk.Canvas(self.master)
-        self.canvas.pack(fill=tk.BOTH, expand=True)
-        self.canvas.bind("<ButtonPress-1>", self.on_click_press)
-        self.canvas.bind("<B1-Motion>", self.on_mouse_drag)
-        self.canvas.bind("<ButtonRelease-1>", self.on_click_release)
+    def load_image(self):
+        file_path = filedialog.askopenfilename(title="Select Image", filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif")])
 
-        self.crop_start = None
-        self.crop_rect_id = None
-
-        menu_bar = tk.Menu(self.master)
-        self.master.config(menu=menu_bar)
-
-        file_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="File", menu=file_menu)
-        file_menu.add_command(label="Open Image", command=self.open_image)
-        file_menu.add_command(label="Crop Image", command=self.crop_image)
-
-    def open_image(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.gif;*.bmp")])
         if file_path:
             self.image_path = file_path
-            self.original_image = Image.open(self.image_path)
             self.display_image()
 
     def display_image(self):
-        if self.original_image:
-            tk_image = ImageTk.PhotoImage(self.original_image)
-            self.canvas.config(width=tk_image.width(), height=tk_image.height())
-            self.canvas.create_image(0, 0, anchor=tk.NW, image=tk_image)
-            self.canvas.tk_image = tk_image
+        image = Image.open(self.image_path)
+        image = image.resize((self.root.winfo_screenwidth(), self.root.winfo_screenheight()))
+        self.current_image = ImageTk.PhotoImage(image)
+        self.image_label.config(image=self.current_image)
 
-    def on_click_press(self, event):
-        self.crop_start = (event.x, event.y)
-
-    def on_mouse_drag(self, event):
-        if self.crop_rect_id:
-            self.canvas.delete(self.crop_rect_id)
-        x, y = self.crop_start
-        x1, y1 = event.x, event.y
-        self.crop_rect_id = self.canvas.create_rectangle(x, y, x1, y1, outline="red")
-
-    def on_click_release(self, event):
-        if self.crop_rect_id:
-            self.canvas.delete(self.crop_rect_id)
-        x, y = self.crop_start
-        x1, y1 = event.x, event.y
-        self.cropped_image = self.crop_image_region(x, y, x1, y1)
-        # You can now use self.cropped_image as a NumPy array or for further processing
-        self.display_cropped_image()
-
-    def crop_image_region(self, x, y, x1, y1):
-        if self.original_image:
-            region = self.original_image.crop((x, y, x1, y1))
-            return np.array(region)
-
-    def display_cropped_image(self):
-        if self.cropped_image is not None:
-            cropped_tk_image = ImageTk.PhotoImage(Image.fromarray(self.cropped_image))
-            self.canvas.config(width=cropped_tk_image.width(), height=cropped_tk_image.height())
-            self.canvas.create_image(0, 0, anchor=tk.NW, image=cropped_tk_image)
-            self.canvas.tk_image = cropped_tk_image
-
-    def crop_image(self):
-        if self.image_path:
-            crop_window = tk.Toplevel(self.master)
-            crop_app = ImageCropperApp(crop_window)
-            crop_app.image_path = self.image_path
-            crop_app.original_image = self.original_image
-            crop_app.display_image()
-
-def main():
-    root = tk.Tk()
-    app = ImageCropperApp(root)
-    root.mainloop()
+    def convert_and_save(self):
+        if self.current_image:
+            image = Image.open(self.image_path)
+            numpy_array = np.array(image)
+            print(numpy_array)
+            # Save the numpy array to a file if needed
 
 if __name__ == "__main__":
-    main()
+    root = tk.Tk()
+    app = ImageViewerApp(root)
+    root.mainloop()
